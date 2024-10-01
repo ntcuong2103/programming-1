fc1c9efaecaafc9367538c44b8c68ab9322dca33
Cuong Tuan Nguyen <37933356+ntcuong2103@users.noreply.github.com>
2024-10-01 08:48:19 +0000

    matrix multiplication

--- code_w3.c
+++ code_w3.c
@@ -16,6 +16,55 @@ int get_max_1d_array(int a[], int N)
 const int num_row = 3;
 const int num_col = 4;
 
+const int num_row_a = 3;
+const int num_col_a = 4;
+const int num_row_b = 4;
+const int num_col_b = 3;
+
+int inner_product(int a[], int b[], int N)
+{
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += a[i] * b[i];
+    }
+    return sum;
+}
+
+void get_row(int a[num_row_a][num_col_a], int row, int result[num_col_a])
+{
+    for (int i = 0; i < num_col_a; i++)
+    {
+        result[i] = a[row][i];
+    }
+}
+
+void get_col(int b[num_row_b][num_col_b], int col, int result[num_row_b])
+{
+    for (int i = 0; i < num_row_b; i++)
+    {
+        result[i] = b[i][col];
+    }
+}
+
+
+
+int matrix_multiplication(int a[num_row_a][num_col_a], int b[num_row_b][num_col_b], int c[num_row_a][num_col_b])
+{
+    for (int i = 0; i < num_row_a; i++)
+    {
+        for (int j = 0; j < num_col_b; j++)
+        {
+            int row[num_col_a];
+            int col[num_row_b];
+            get_row(a, i, row);
+            get_col(b, j, col);
+            c[i][j] = inner_product(row, col, num_col_a);
+        }
+    }
+}
+
+
 int get_max_2d_array(int a[num_row][num_col], int axis)
 {
     // your code here
@@ -39,40 +88,63 @@ int get_max_2d_array(int a[num_row][num_col], int axis)
     if (axis == 1)
     {
         // find max in each row and print out
+        for (int i = 0; i < num_row; i++)
+        {
+            int max = a[i][0];
+            for (int j = 1; j < num_col; j++)
+            {
+                if (a[i][j] > max)
+                {
+                    max = a[i][j];
+                }
+            }
+            printf("Max in row %d is %d\n", i, max);
+        }
     }
 }
 
 
 
+
 int main()
 {
-    // int counter = 0;
-    // for (int i = 0; i < 10; i++)
-    // {
-    //     if (i < 5) continue;
-    //     counter += 1;
-    // }
-    // // counter from 5 -> 9
-    // printf("counter = %d \n", counter);
+    // create 2d array 3x4
 
-    // create array 10x10 with value == index
-    int arr[10][10];
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            arr[i][j] = i * 10 + j;
-        }
-    }
+    // int a[3][4] = {
+    //     {1, 2, 3, 4},
+    //     {5, 6, 7, 8},
+    //     {9, 10, 11, 12}
+    // };
 
-    // loop through a column
-    int col_index = 3;
+    // get_max_2d_array(a, 0);
 
-    for (int i = 0; i < 10; i ++)
-    {
-        printf("arr[%d][%d] = %d \n", i, col_index, arr[i][col_index]);
-    }
+    // get_max_2d_array(a, 1);
 
+
+    // const int N = 9;
+    // int a[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // int b[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    // int result = inner_product(a, b, N);
+
+    // printf("Inner product of a and b is %d\n", result);
+
+    // create two matrix a, b
+    int a[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+
+    int b[4][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+        {10, 11, 12},
+    };
+
+    int result[3][3];
+    matrix_multiplication(a, b, result);
 
     return 0;
 }
