044d2e3c0b2be0dff1e73725ce80f45bb7f83d9d
Cuong Tuan Nguyen <37933356+ntcuong2103@users.noreply.github.com>
2024-10-15 08:52:20 +0000

    code w5 updated

--- code_w5.c
+++ code_w5.c
@@ -67,52 +67,76 @@ int * max_of_2d_array(int ** int_arr, size_t num_row, size_t num_col, int axis)
     return max_arr;
 }
 
+int swap(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+void func_try_modify_var(int* p_a)
+{
+    *p_a = 100;
+}
+
 int main()
 {
-    int *int_arr = (int *)malloc(5 * sizeof(int));
+    int a = 10;
+    int b = 20;
+    swap(a, b);
+    printf("a is %d, b is %d\n", a, b);
+    func_try_modify_var(&a);
+    printf("a is %d\n", a);
+    
+    // int *int_arr = (int *)malloc(5 * sizeof(int));
 
-    printf("size of unsigned long is %d\n", sizeof(size_t));
+    // printf("size of unsigned long is %d\n", sizeof(size_t));
 
-    int_arr[0] = 1;
-    int_arr[1] = 2;
-    int_arr[2] = 3;
-    int_arr[3] = 4;
-    int_arr[4] = 5;
+    // int_arr[0] = 1;
+    // int_arr[1] = 2;
+    // int_arr[2] = 3;
+    // int_arr[3] = 4;
+    // int_arr[4] = 5;
 
-    // int_arr = realloc(int_arr, 3 * sizeof(int));
+    // // int_arr = realloc(int_arr, 3 * sizeof(int));
 
-    // free(int_arr);
+    // // free(int_arr);
 
-    // int_arr[0]; // undefined behavior
-    printf("max of array is %d\n", max_of_array(int_arr, 5));
-    reverse_array(int_arr, 5);
+    // // int_arr[0]; // undefined behavior
+    // printf("max of array is %d\n", max_of_array(int_arr, 5));
+    // reverse_array(int_arr, 5);
 
-    // allocate 2d array of 3x4 and assign values
-    int n_rows = 3;
-    int n_cols = 4;
+    // // allocate 2d array of 3x4 and assign values
+    // int n_rows = 3;
+    // int n_cols = 4;
 
-    int **int_arr_2d = (int **)malloc(n_rows * sizeof(int *));
-    for (size_t i = 0; i < n_rows; i++)
-    {
-        int_arr_2d[i] = (int *)malloc(n_cols * sizeof(int));
+    // int **int_arr_2d = (int **)malloc(n_rows * sizeof(int *));
+    // for (size_t i = 0; i < n_rows; i++)
+    // {
+    //     int_arr_2d[i] = (int *)malloc(n_cols * sizeof(int));
 
-        for (size_t j = 0; j < n_cols; j++)
-        {
-            int_arr_2d[i][j] = i * n_cols + j;
-        }
-    }
+    //     for (size_t j = 0; j < n_cols; j++)
+    //     {
+    //         int_arr_2d[i][j] = i * n_cols + j;
+    //     }
+    // }
 
-    int *max_arr = max_of_2d_array(int_arr_2d, n_rows, n_cols, 0);
-    for (size_t i = 0; i < n_cols; i++)
-    {
-        printf("max of column %d is %d\n", i, max_arr[i]);
-    }
+    // int *max_arr = max_of_2d_array(int_arr_2d, n_rows, n_cols, 0);
+    // for (size_t i = 0; i < n_cols; i++)
+    // {
+    //     printf("max of column %d is %d\n", i, max_arr[i]);
+    // }
 
-    int *max_arr2 = max_of_2d_array(int_arr_2d, n_rows, n_cols, 1);
-    for (size_t i = 0; i < n_rows; i++)
-    {
-        printf("max of row %d is %d\n", i, max_arr2[i]);
-    }
+    // int *max_arr2 = max_of_2d_array(int_arr_2d, n_rows, n_cols, 1);
+    // for (size_t i = 0; i < n_rows; i++)
+    // {
+    //     printf("max of row %d is %d\n", i, max_arr2[i]);
+    // }
 
     return 0;
 }
