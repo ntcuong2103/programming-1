bf8dc22923059783b99fee9e51675e4a1dd040a7
Cuong Tuan Nguyen <37933356+ntcuong2103@users.noreply.github.com>
2024-11-12 03:01:20 +0000

    lab w6 multi array

--- lab_w6/multi_array.c
+++ lab_w6/multi_array.c
@@ -12,8 +12,18 @@ struct MultiArray
  *
  * array [2][1] = get_data(array, [2, 1])
 */
+
 int get_data(struct MultiArray* array, int* indexes, int n_dims)
 {
+    int flat_index = 0;
+    int cumul_shape = 1;
+    for (int i = n_dims - 1; i >= 0; i--)
+    {
+        flat_index += indexes[i] * cumul_shape;
+        cumul_shape *= array->shape[i];
+    }
+
+    return array->data[flat_index];
 }
 
 int main()
@@ -30,7 +40,7 @@ int main()
     for (int i = 0; i < 3 * 2 * 2; i++)
     {
         array->data[i] = i;
-        // printf("array->data[%d] = %d\n", i, array->data[i]);
+        printf("array->data[%d] = %d\n", i, array->data[i]);
     }
 
     // assign shape
@@ -41,6 +51,21 @@ int main()
 
     int indexes[] = { 1, 0, 1 };
     int out = get_data(array, indexes, 3);
+    printf("out = %d\n", out);
+
+    // loop through all elements
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k = 0; k < 2; k++)
+            {
+                int indexes[] = { i, j, k };
+                int out = get_data(array, indexes, 3);
+                printf("array[%d][%d][%d] = %d\n", i, j, k, out);
+            }
+        }
+    }
 
     return 0;
 }
