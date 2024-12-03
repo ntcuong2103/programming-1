391a3ff7a9717e6e78e8442ebe3c3839a15d2bed
Cuong Tuan Nguyen <37933356+ntcuong2103@users.noreply.github.com>
2024-12-03 08:56:43 +0000

    lab w10

--- lab_w10/datetime.c
+++ lab_w10/datetime.c
@@ -2,12 +2,12 @@
 
 void sprint_time(char* outstr, struct tm* info)
 {
-
+    sprintf(outstr, "%02d:%02d:%02d", info->tm_hour, info->tm_min, info->tm_sec);
 }
 
 void sprint_date(char* outstr, struct tm* info)
 {
-
+    sprintf(outstr, "%02d/%02d/%04d", info->tm_mday, info->tm_mon + 1, info->tm_year + 1900);
 }
 
 int main()
@@ -26,6 +26,29 @@ int main()
     info->tm_year + 1900; // year
 
     info->tm_wday; // day of the week: 0 -> Sun, 1 -> Mon, ...
+
+    char *days[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+    // print
+    printf("Current time is: %02d:%02d:%02d\n", info->tm_hour, info->tm_min, info->tm_sec);
+    printf("Today is: %s, %02d/%02d/%04d\n", days[info->tm_wday], info->tm_mday, info->tm_mon + 1, info->tm_year + 1900);
+
+    // char outstr[100]; // static memory allocation
+    char *outstr = (char*)malloc(100); // dynamic memory allocation
+    sprint_time(outstr, info);
+    printf("Current time is: %s\n", outstr);
+
+
+    // string concatenation using sprintf
+    char *outconcat = (char*)malloc(1000);
+    sprintf(outconcat, "Current time is: %s", outstr);
+    // get date
+    sprint_date(outstr, info);
+
+    sprintf(outconcat, "%s, and today is: %s", outconcat, outstr);
+    printf("%s\n", outconcat);
+
+    
 
     return 0;
 }
--- lab_w10/read_ink.c
+++ lab_w10/read_ink.c
@@ -48,20 +48,86 @@ int main(int argc, char* argv[])
 
     char s[100];
 
+    Point p_list[1000];
+
+    int index = 0;
     while (fgets(s, 100, f) != NULL)
     {
         printf("%s\n", s);
+        // check if begin with "["
+        if (s[0] == '[')
+        {
+        }
+        else
+        {
+            // get the point
+            // Point p;
+            int attr;
+            sscanf(s, "%d %d %d", &attr, &p_list[index].x, &p_list[index].y);
+            index++;
+        }
     }
+
+    fclose(f);
+
 
     int width = 50;
     int height = 20;
 
-    char** data = malloc(height * sizeof(char*));
+    int** data = malloc(height * sizeof(int*));
 
     for (int i = 0; i < height; i++)
     {
-        data[i] = malloc(width * sizeof(char));
-        memset(data[i], 1, width);
+        data[i] = malloc(width * sizeof(int));
+        memset(data[i], 0, width);
+    }
+
+    // scale the points to width and height
+    // int width, height, offset_x, offset_y;
+    int max_x, max_y, min_x, min_y;
+
+    for (int i = 0; i < index; i++)
+    {
+        if (i == 0)
+        {
+            max_x = min_x = p_list[i].x;
+            max_y = min_y = p_list[i].y;
+        }
+        else
+        {
+            if (p_list[i].x > max_x)
+            {
+                max_x = p_list[i].x;
+            }
+            if (p_list[i].x < min_x)
+            {
+                min_x = p_list[i].x;
+            }
+            if (p_list[i].y > max_y)
+            {
+                max_y = p_list[i].y;
+            }
+            if (p_list[i].y < min_y)
+            {
+                min_y = p_list[i].y;
+            }
+        }
+    }
+
+    int scale_x = width / (max_x - min_x + 1);
+    int scale_y = height / (max_y - min_y + 1);
+    int offset_x = -min_x;
+    int offset_y = -min_y;
+
+    for (int i = 0; i < index; i++)
+    {
+        p_list[i].x = p_list[i].x * scale_x + offset_x;
+        p_list[i].y = p_list[i].y * scale_y + offset_y;
+    }
+
+    for (int i = 0; i < index; i++)
+    {
+        data[p_list[i].y][p_list[i].x] = 1;
     }
 
     draw_to_screen(data, height, width);
